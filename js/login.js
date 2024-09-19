fb7e0c92520b5351a97203f4f597beec4a7fb3fe
Sahadat Nayem <sahadatnayem333@gmail.com>
2024-09-19 15:16:33 +0600

    added code

--- js/login.js
+++ js/login.js
@@ -1,4 +1,12 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
     const phoneNumber = document.getElementById('phone-number').value;
-})
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
+});
