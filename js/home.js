d644fea419522cc496e02384e09a1c61e7d236ff
Sahadat Nayem <sahadatnayem333@gmail.com>
2024-09-19 16:14:07 +0600

    added add money code

---
+++ js/home.js
@@ -0,0 +1,9 @@
+// Add money to the account
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+});
